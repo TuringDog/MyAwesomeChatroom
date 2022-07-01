c1c4ec29b5af53d921d8563f73e1af887d6e251c
Jeffrey0Liao <scycl2@nottingham.edu.cn>
2022-07-01 00:23:34 -0400

    add feeding system and user profile.

--- mychatroom/base/models.py
+++ mychatroom/base/models.py
@@ -13,7 +13,7 @@ class Room(models.Model):
     topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name=models.CharField(max_length=200)
     description=models.TextField(null=True, blank=True)
-    #participant
+    participants=models.ManyToManyField(User, related_name='participants', blank=True)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -30,5 +30,8 @@ class Message(models.Model):
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering=['-updated', '-created']
+        
     def __str__(self) -> str:
         return self.body[0:50]
--- mychatroom/base/views.py
+++ mychatroom/base/views.py
@@ -57,20 +57,31 @@ def home(request):
         )
     room_count=rooms.count()
     topics=Topic.objects.all()
-    context={'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    room_messages=Message.objects.all()[:5]
+    context={'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
+
+def userProfile(request, pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_messages=user.message_set.all()[:5]
+    topics=Topic.objects.all()
+    context={'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 def room(request, pk):
     room=Room.objects.get(id=pk) 
-    room_messages=room.message_set.all().order_by('-created')
+    room_messages=room.message_set.all()
+    participants=room.participants.all()
     if request.method=='POST':
         message=Message.objects.create(
             user=request.user, 
             room=room, 
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context={'room':room, 'room_messages':room_messages}
+    context={'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
@@ -107,3 +118,13 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk):
+    message=Message.objects.get(id=pk)
+    if request.user!=message.user:
+        return HttpResponse('Access denied.')
+    if request.method=="POST":
+        message.delete()
+        return redirect('room', pk=message.room.id)
+    return render(request, 'base/delete.html', {'obj':message})
