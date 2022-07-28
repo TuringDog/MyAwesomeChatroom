02c6b783f5c29864670bb8ae3080177eef105337
Jeffrey0Liao <scycl2@nottingham.edu.cn>
2022-07-28 18:46:34 -0400

    fix bulkWriteError when sending messages.

--- mychatroom/base/views.py
+++ mychatroom/base/views.py
@@ -80,7 +80,8 @@ def room(request, pk):
             room=room, 
             body=request.POST.get('body')
         )
-        room.participants.add(request.user)
+        if request.user not in participants:
+            room.participants.add(request.user)
         return redirect('room', pk=room.id)
     context={'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
