364702d719482b4c8c56a253f6c076b4096c6dfb
Jeffrey0Liao <scycl2@nottingham.edu.cn>
2022-07-29 19:10:00 -0400

    room messages display & some restylings

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import RoomList from "../../components/RoomList";
 import TopicList from "../../components/TopicList";
 import MessageList from "../../components/messageList";
+import Navbar from "../../components/Navbar";
 
 import './home.css'
 
 export default function HomePage(){
     return (
         <div>
-            <h1>MY HOME PAGE</h1>
-            <TopicList />
-            <RoomList />
-            <MessageList />
+            <div>
+                <Navbar />
+                <h1>Welcome to Turing Dog</h1>
+            </div>
+            <div className="home-content">
+                <div>
+                    <h3>Hot Topics</h3>
+                    <TopicList />
+                </div>
+                <div>
+                    <h3>Rooms</h3>
+                    <RoomList />
+                </div>
+                <div>
+                    <h3>Just Happened</h3>
+                    <MessageList />
+                </div>
+            </div>
+            
         </div>
     );
 }
